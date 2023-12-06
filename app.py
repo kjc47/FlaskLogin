78faf91d1e8a979e82f6dc51033d5a415c163528
Kevin Chavez <kjc47@njit.edu>
2023-12-06 10:00:44 -0500

    commit

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
-import yaml
+
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask('__name__')
+app.secret_key = 'your_secret_key'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
 
 
 # Load database configuration from a YAML file or set your database config here
-db = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
+
+app.config['MYSQL_HOST'] = 'kjc47@webhost01.arcs.njit.edu'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'kjc47'
 
 mysql = MySQL(app)
 
-app.secret_key = 'your_secret_key'  # Change this to a random secret key
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    if 'username' in session:
+        return render_template('home.html', username=session['username'])
+    else:
+        return render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
-        cursor = mysql.connection.cursor
+        pwd = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT username, password FROM tbl_users WHERE username = %s", [username])
+        user = cur.fetchone()
+        cur.close()
+
+        if user and pwd == user[1]:
+            session['username'] = user[0]
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+
+    return render_template('login.html')
